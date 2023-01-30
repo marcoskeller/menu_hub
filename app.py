b451224ab4e7c10fac4e12fa01ea4039120dfc9b
Marcos Keller <marcoskeller@hotmail.com>
2023-01-30 12:26:01 -0300

    Criacao do arquivo inicial e instalacao do Boostrap

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+import constantes
+
+from flask_session import Session
+from flask_bootstrap import Bootstrap5
+from flask import Flask, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(basedir, constantes.DATABASE_NAME)}"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+bootstrap = Bootstrap5(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
---
+++ constantes.py
@@ -0,0 +1,32 @@
+DATABASE_NAME = 'menuhub.sqlite'
+
+PLATES_TABLE_NAME = 'pratos'
+USERS_TABLE_NAME = 'usuarios'
+RESTAURANTS_TABLE_NAME = 'restaurantes'
+
+ID_PAGE_PLATES = 'pratos.html'
+ID_PAGE_LOGIN = 'login.html'
+ID_PAGE_SEARCH = 'busca.html'
+ID_PAGE_USER = 'adicionarUsuario.html'
+ID_PAGE_RESTAURANT = 'restaurante.html'
+
+ID_ROUTE_PLATES = '/pratos'
+ID_ROUTE_LOGIN = '/login'
+ID_ROUTE_LOGOUT = '/logout'
+ID_ROUTE_SEARCH = '/busca'
+ID_ROUTE_USER = '/adicionarUsuario'
+ID_ROUTE_RESTAURANT = '/restaurante'
+
+MESSAGE_ERROR_SAVING_USER = 'Ocorreu um erro ao tentar salvar um novo usuário.'
+MESSAGE_ERROR_SAVING_PLATE = 'Ocorreu um erro ao tentar salvar o prato.'
+MESSAGE_ERROR_DELETING_PLATE = 'Ocorreu um erro ao excluir um prato.'
+MESSAGE_ERROR_EDITING_PLATE = 'Ocorreu um erro ao editar um prato.'
+MESSAGE_ERROR_SAVING_RESTAURANT = 'Ocorreu um erro ao tentar salvar um novo restaurante.'
+MESSAGE_ERROR_INVALID_CREDENTIALS = 'Credenciais inválidas.'
+MESSAGE_ERROR_NOT_VALID_PARAMETER = 'Erro! Não é um parâmetro válido.'
+MESSAGE_NOTHING_FOUND = 'Nada encontrado'
+
+MESSAGE_LOG_OUT = 'Você foi desconectado.'
+MESSAGE_PLEASE_LOG_IN = 'Por favor faça login'
+
+SESSION_ID = 'logged_user'
